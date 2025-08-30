7283577b1dae7bea1eebe375a475924d3ee7424f
unknown <6749010011@phuketvc.ac.th>
2025-08-30 14:50:44 +0700

    step2

---
+++ lib/frist_screen.dart
@@ -0,0 +1,77 @@
+// Step 2: Install loading app screen
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import "package:flutter_spinkit/flutter_spinkit.dart";
+import 'dart:async';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(
+      const Duration(seconds: 3),
+      () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const SecondScreen()),
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.purple, Color.fromARGB(255, 58, 62, 183)],
+          begin: FractionalOffset(0, 0),
+          end: FractionalOffset(0.5, 0.6),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              '.android\\assets\\image\\app_screen.png',
+              width: 200,
+            ),
+          ),
+
+          const SizedBox(height: 20),
+          const SpinKitSpinningLines(color: Colors.pinkAccent),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Screen')),
+      body: const Center(
+        child: Text(
+          'This is the Second Screen!',
+          style: TextStyle(
+            fontSize: 24,
+            color: Color.fromARGB(255, 201, 236, 138),
+            fontWeight: FontWeight.w500,
+            fontFamily: "Alike",
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,8 @@
 import 'package:flutter/material.dart';
 
+// step2 Install loading app screen
+import 'package:learn_flutter_67_1_2/frist_screen.dart';
+
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Change the Icon!'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+  runApp(const MaterialApp(home: FirstScreen()));
 }
